9ee761c816cc393ad21dc5ff0d8aad96c13714c6
Mozgus <retailnet@rambler.ru>
2015-11-18 21:14:28 +0300

    Now the circles become colored

--- app/src/main/java/com/berryjam/wildcircles/EnemyCircle.java
+++ app/src/main/java/com/berryjam/wildcircles/EnemyCircle.java
@@ -1,11 +1,14 @@
 package com.berryjam.wildcircles;
 
+import android.graphics.Color;
+
 import java.util.Random;
 
 public class EnemyCircle extends SimpleCircle {
 
     public static final int FROM_RADIUS = 10;
     public static final int TO_RADIUS = 110;
+    public static final int ENEMY_COLOR = Color.RED;
 
     public EnemyCircle(int x, int y, int radius) {
         super(x, y, radius);
@@ -16,6 +19,8 @@ public class EnemyCircle extends SimpleCircle {
         int x = random.nextInt(GameManager.getWidth());
         int y = random.nextInt(GameManager.getHeight());
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
-        return new EnemyCircle(x, y, radius);
+        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
+        enemyCircle.setColor(ENEMY_COLOR);
+        return enemyCircle;
     }
 }
